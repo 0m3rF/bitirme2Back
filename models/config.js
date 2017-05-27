9fd0a9580dc6d85d599d5f74b9ca2be510440e52
Ömer Faruk Kırlı <omerfarukkirli1994@gmail.com>
2017-05-28 02:38:16 +0300

    Son ayarlamalar yapılıyor

--- models/config.js
+++ models/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	'url' : "mongodb://alikemal:123456alikemal@139.162.158.192:27017/admin",
+	'url' : "mongodb://alikemal:123456alikemal@139.162.158.192:27017/admin?authMechanism=SCRAM-SHA-1",
 	'userCollection' : "user",
-	'historyCollection' : "historySongs"
+	'songsCollection' : "songs",
+	'historyCollection' : "historySongs",
+	'recommendCollection' : "recommend"
 }
